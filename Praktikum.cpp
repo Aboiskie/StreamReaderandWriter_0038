aa8ac96553d73c23248a85a545687ef0b8a10fc9
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-06-01 12:35:03 +0700

    Menambahkan method ambilProduk untuk mengambil produk dari etalase dengan penanganan error

--- Praktikum.cpp
+++ Praktikum.cpp
@@ -23,4 +23,19 @@ public:
         etalase[2] = "Printer Epson";
     }
 
-    
+    // Method mengambil produk
+    string ambilProduk(size_t nomorRak) {
+        try {
+            // Wajib menggunakan .at()
+            return etalase.at(nomorRak);
+        }
+        catch (const out_of_range&) {
+            // Throw custom error
+            throw runtime_error(
+                "Gagal Mengambil Barang : Rak nomor " +
+                to_string(nomorRak) +
+                " kosong atau tidak tersedia!"
+            );
+        }
+    }
+};
