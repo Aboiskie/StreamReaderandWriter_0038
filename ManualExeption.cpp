83079aff3a5d18f62eabd072f6ecee84ee647350
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-06-01 11:08:21 +0700

    menambahkan pernyataan output tambahan di dalam blok try pada fungsi main

--- ManualExeption.cpp
+++ ManualExeption.cpp
@@ -8,4 +8,6 @@ int main()
     {
         cout << "Selamat belajar di prodi TI UMY" << endl;
         //throw 0.5; //melemparkan sebuah integer maka
-        
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+    
