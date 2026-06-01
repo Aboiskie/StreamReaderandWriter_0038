facdb66c93f07a05ffa0926df103504280fba789
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-06-01 11:08:02 +0700

    menambahkan blok try untuk pengujian error dalam fungsi main

--- ManualExeption.cpp
+++ ManualExeption.cpp
@@ -2,3 +2,10 @@
 using namespace std;
 
 int main()
+{
+    //membuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integer maka
+        
